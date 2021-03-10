351972cc40d960df0d722348f96ea6a541e8e6f5
AdamZsofi <adamzsofi.mail@gmail.com>
2021-03-10 22:54:20 +0100

    Exercise 3 done

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,9 +1,10 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
@@ -12,20 +13,45 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+
+		Scanner scanner = new Scanner(System.in);
+		String input;
+		boolean shouldExit = false;
+		
+		while(true) {
+			input = scanner.nextLine(); 
+			switch(input) {
+			case "start":
+				s.raiseStart();
+				break;
+			case "black":
+				s.raiseBlack();
+				break;
+			case "white":
+				s.raiseWhite();
+				break;
+			case "exit":
+				shouldExit = true;
+				break;
+			default:
+				System.out.println("Unknown command");
+				break;
+			}
+			
+			if(shouldExit) { 
+				break;
+			}
+			s.runCycle();
+			print(s);
+		}
+		
+		System.exit(0);
 	}
 
 	public static void print(IExampleStatemachine s) {
